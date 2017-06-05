b7d175c09e69c54d4cf1b4ba8c0a6f8b376f28a7
Neveralso <neveralso@gmail.com>
2017-06-05 15:20:38 +0800

    commit

--- sample_test.sh
+++ sample_test.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_cpl.cpl
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_jar.jar
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_pdf.pdf
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_php.php --package php
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_python.py
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_vbs.vbs
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_excel.xlsx --timeout 30
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_word.docx --timeout 30
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_ppt.pptx --timeout 30
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_zip.zip -o file=main.exe
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/dll/libtest_dll.dll -o function=dll_entry,arguments="123 32"
-$PY cuckoo.py submit $CUCKOO_TEST_SAMPLE_DIR/test_html.html
+if [ "$1" != "" ]; then
+	echo "Positional parameter 1 contains something"
+	exit
+fi
 
+python cuckoo.py submit $1/test_cpl.cpl
+python cuckoo.py submit $1/test_jar.jar
+python cuckoo.py submit $1/test_pdf.pdf
+python cuckoo.py submit $1/test_php.php --package php
+python cuckoo.py submit $1/test_python.py
+python cuckoo.py submit $1/test_vbs.vbs
+python cuckoo.py submit $1/test_excel.xlsx --timeout 30
+python cuckoo.py submit $1/test_word.docx --timeout 30
+python cuckoo.py submit $1/test_ppt.pptx --timeout 30
+python cuckoo.py submit $1/test_zip.zip -o file=main.exe
+python cuckoo.py submit $1/dll/libtest_dll.dll -o function=dll_entry,arguments="123 32"
+python cuckoo.py submit $1/test_html.html
 
-$PY cuckoo.py submit -u http://192.168.56.1:38080/
+python cuckoo.py submit $1/DANGER/StuxNet_infected.zip -o password=infected,file=malware.exe
+python cuckoo.py submit $1/DANGER/VirusShare_84c82835a5d21bbcf75a61706d8ab549_Wanncry_infected.zip -o password=infected
+
+python cuckoo.py submit -u http://192.168.56.1:38080/

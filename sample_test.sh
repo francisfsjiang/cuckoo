ba8c902d9ff33cc09042ce4360700a5c6dfed0c9
Neveralso <neveralso@gmail.com>
2017-06-06 10:15:46 +0800

    commit

--- sample_test.sh
+++ sample_test.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-if [ "$1" != "" ]; then
-	echo "Positional parameter 1 contains something"
-	exit
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 DIRECTORY" >&2
+    exit 1
+fi
+if ! [ -e "$1" ]; then
+    echo "$1 not found" >&2
+    exit 1
+fi
+if ! [ -d "$1" ]; then
+    echo "$1 not a directory" >&2
+    exit 1
 fi
 
 python cuckoo.py submit $1/test_cpl.cpl
